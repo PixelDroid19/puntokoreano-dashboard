db72ca7de088f6ce879fc5e1058a82d0bd165aed
dmonasterios <monasterio13septiembre@gmail.com>
2025-06-30 14:35:31 -0500

    refactor(sider-menu, index.css): actualizar estilos y fuentes en el menú lateral
    Se han realizado cambios en el componente `SiderMenu`, actualizando la imagen del logo y ajustando el tamaño. Además, se ha añadido una nueva fuente "Karate" en el archivo `index.css`, mejorando la estética del menú lateral. También se han estandarizado las propiedades CSS para una mejor legibilidad y mantenimiento del código. Estos cambios contribuyen a una experiencia de usuario más atractiva y coherente en la interfaz.

--- src/components/shared/layout/sider/SiderMenu.component.tsx
+++ src/components/shared/layout/sider/SiderMenu.component.tsx
@@ -1,18 +1,28 @@
 import { Flex } from "antd";
 import MenuCollapsed from "../menu/Menu.component";
 import Sider from "antd/es/layout/Sider";
-import Logo from "/src/assets/logo-2.png";
+import Logo from "/src/assets/logo-512x512.png";
 
 const SiderMenu = () => {
-
-    return (
-        <Sider theme='light' style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }} >
-            <Flex justify='center'>
-                <img src={Logo} width={100} style={{ marginTop: 10 }} />
-            </Flex>
-            <MenuCollapsed  />
-        </Sider>
-    )
-}
+  return (
+    <Sider
+      theme="light"
+      style={{
+        overflow: "auto",
+        height: "100vh",
+        position: "fixed",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <Flex justify="center" className="flex-col items-center">
+        <img src={Logo} width={60} style={{ marginTop: 10 }} />
+        <p style={{ fontFamily: "Karate", fontSize: 23 }}>Punto Koreano</p>
+      </Flex>
+      <MenuCollapsed />
+    </Sider>
+  );
+};
 
 export default SiderMenu;
